bd6eaf6c8586389db351f43ab72b7845e6993aac
kananunnaunta <s6603052413108@email.kmutnb.ac.th>
2025-02-19 14:01:36 +0700

    nav

---
+++ src/router2.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, NavLink } from "react-router-dom";
+import './navlink.css'
+
+export default function Router2() {
+    return(
+        <BrowserRouter>
+            <nav className="nav">   
+            
+                <NavLink to='/'  className={({isActive})=>isActive?"active_menu":"menu"}
+                style={({isActive})=>{
+                    return{
+                        fontWeight:isActive?"bold":""
+                    }
+                }}>Home </NavLink> -&nbsp;
+                <NavLink to='/product' className={({isActive})=>isActive?"active+menu":"menu"}>Product</NavLink>
+
+                <NavLink to='/member' className={({isActive})=>isActive?"active+menu":"menu"}>Member</NavLink>
+
+                <NavLink to='/contact' className={({isActive})=>isActive?"active+menu":"menu"}>Contact Us</NavLink>
+            
+            </nav>
+        </BrowserRouter>
+    )
+}
+
+
+
