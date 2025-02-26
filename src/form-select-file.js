dedaec7d82e6f8774b3777b274692e5d25bab9dd
kananunnaunta <s6603052413108@email.kmutnb.ac.th>
2025-02-26 15:42:30 +0700

    Update form-select-file.js

--- src/form-select-file.js
+++ src/form-select-file.js
@@ -8,7 +8,7 @@ export default function FormSelectFile() {
     //เมื่อคลิกปุ่ม ให้น าค่าจาก select ทั้งสองอันมาเป็นเงื่อนไขตรวจสอบไฟล์
     const onClickButton = () => {
         let maxNumFiles = selectMaxNumFiles.current.value
-        let maxSize =  selectMaxSize.current.value
+        let maxSize = selectMaxSize.current.value
         //ตรวจสอบจ านวนไฟล์
         if (inputFile.current.files.length > maxNumFiles) {
             alert(`เลือกได้ไม่เกิน ${maxNumFiles} ไฟล์`)
@@ -24,40 +24,39 @@ export default function FormSelectFile() {
         alert('Files OK')
     }
     return (
-<div className="mt-4 mx-auto p-3 rounded"
-    style={{ width:'400px', background:'#cee'}}>
-<form>
-<div className="form-group mb-3">
-<label htmlFor="maxNumFiles" className="form-label">จำนวนไฟล์สูงสุด</label>
-<select id="maxNumFiles" className="form-select form-select-sm" ref={maxNumFiles}>
-{
-maxNumFiles.map(i => {
-return <option value={i}>{i}</option>
-})
-}
-</select>
-</div>
-<div className="form-group mb-3">
-<label htmlFor="maxFilesSize" className="form-label">ขนาดไฟล์สูงสุด (KB)</label>
-<select id="maxFileSize" className="form-select form-select-sm" ref={selectMaxSize}>
-{
-maxSize.map(i => {
-return <option value={i}>{i}</option>
-})
-}
-</select>
-</div>
-<div className="form-group mb-3">
-<label htmlFor="file" className="file">เลือกไฟล์</label>
-<input type="file" id="file" accept="image/*"
-className="file form-control-sm" ref={inputFile} multiple/>
-</div>
-<div className="text-center mt-4">
-<button type="button" className="btn btn-sm btn-primary px-4"
-onClick={onClickButton}>OK
-</button>
-</div >
-</form >
-</div >
-)
+        <div className="mt-4 mx-auto p-3 rounded"
+            style={{ width: '400px', background: '#cee' }}>
+            <form>
+                <div className="form-group mb-3">
+                    <label htmlFor="maxNumFiles" className="form-label">จำนวนไฟล์สูงสุด</label>
+                    <select id="maxNumFiles" className="form-select form-select-sm" ref={maxNumFiles}>
+                        {
+                            maxNumFiles.map(i => {
+                                return <option value={i}>{i}</option>
+                            })
+                        }
+                    </select>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="maxFilesSize" className="form-label">ขนาดไฟล์สูงสุด (KB)</label>
+                    <select id="maxFileSize" className="form-select form-select-sm" ref={selectMaxSize}>
+                        {
+                            maxSize.map(i => {
+                                return <option value={i}>{i}</option>
+                            })
+                        }
+                    </select>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="file" className="form-label">เลือกไฟล์</label>
+                    <input type="file" id="file" accept="image/*" className="form-control form-control-sm" ref={inputFile} multiple />
+                </div>
+                <div className="text-center mt-4">
+                    <button type="button" className="btn btn-sm btn-primary px-4" onClick={onClickButton}>
+                        OK
+                    </button>
+                </div >
+            </form >
+        </div >
+    )
 }
